4a95571c8f6ed2f24661a159abc58faa8859d287
dimitri <dimitriromaric009@gmail.com>
2021-06-30 03:50:04 +0200

    new feature : save image

--- BitmapImage.cpp
+++ BitmapImage.cpp
@@ -19,3 +19,14 @@ bool BitmapImage::load(std::string name) {
 BitmapImage::~BitmapImage() {
     delete[] buffer;
 }
+
+bool BitmapImage::save(std::string name) {
+    // check it is a supported image file
+    std::string fileExt = name.substr(name.find_last_of(".") + 1);
+    if (fileExt == "jpg") {
+        // load image file
+        // decompress into buffer
+        return true;
+    }
+    return false;
+}
--- main.cpp
+++ main.cpp
@@ -6,8 +6,11 @@ int main() {
     std::cout << "Image viewer" << std::endl;
     BitmapImage b;
     std::string imgName = "test.png";
-    if (b.load(imgName))
+    if (b.load(imgName)) {
         std::cout << "show image: " << imgName << std::endl;
+        //... change image ...
+        b.save(imgName);
+    }
     else
         std::cerr << "image:" << imgName << " not readable." << std::endl;
     return 0;
