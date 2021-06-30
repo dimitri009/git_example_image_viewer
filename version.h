454bb7ae4b51267cedb98a252be6e8332c97153c
dimitri <dimitriromaric009@gmail.com>
2021-06-30 04:21:30 +0200

    Rilascio v1.0

---
+++ version.h
@@ -0,0 +1,10 @@
+//
+// Created by dimit on 30/06/2021.
+//
+
+#ifndef GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
+#define GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
+
+const std:: string VERSION = "1.0";
+
+#endif //GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
